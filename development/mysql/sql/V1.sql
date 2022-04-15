7c31ea2a16f13c88444785efb8ddc7771e1238e2
Akihide Ito <akky.supernova@gmail.com>
2022-04-16 01:01:42 +0900

    feat: recordにsta,c_by,u_at,record_idの複合インデックス

--- development/mysql/sql/V1.sql
+++ development/mysql/sql/V1.sql
@@ -1,2 +1,4 @@
 CREATE INDEX `status_updated_at_idx` ON `record` (`status`, `updated_at` DESC, `record_id` ASC);
 CREATE INDEX `linked_record_id_created_at_idx` ON `record_comment` (`linked_record_id`, `created_at` DESC);
+
+CREATE INDEX `sta_c_by_u_at_record_id` ON `record` (`status`, `created_by`, `updated_at` DESC, `record_id` ASC);
