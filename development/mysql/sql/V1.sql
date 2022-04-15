7999eada30066efb6307a9f2162d94d36d3ad11b
Akihide Ito <akky.supernova@gmail.com>
2022-04-16 01:04:02 +0900

    feat: linked_record_id, item_idの複合インデックス

--- development/mysql/sql/V1.sql
+++ development/mysql/sql/V1.sql
@@ -1,4 +1,9 @@
 CREATE INDEX `status_updated_at_idx` ON `record` (`status`, `updated_at` DESC, `record_id` ASC);
 CREATE INDEX `linked_record_id_created_at_idx` ON `record_comment` (`linked_record_id`, `created_at` DESC);
-
-CREATE INDEX `sta_c_by_u_at_record_id` ON `record` (`status`, `created_by`, `updated_at` DESC, `record_id` ASC);
+CREATE INDEX `sta_c_by_u_at_record_id` ON `record` (
+  `status`,
+  `created_by`,
+  `updated_at` DESC,
+  `record_id` ASC
+);
+CREATE INDEX `sta_c_by_u_at_record_id` ON `record_item_file` (`linked_record_id`, `item_id` ASC);
